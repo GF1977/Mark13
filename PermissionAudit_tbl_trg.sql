2f525b2241c678773511cc60bdfa9213fe20bbb7
GF1977 <gloom.fox@yandex.ru>
2018-11-04 20:07:02 +0100

    Trigger & table, version 0.2

--- PermissionAudit_tbl_trg.sql
+++ PermissionAudit_tbl_trg.sql
@@ -1,3 +1,7 @@
+
+USE csn_dba
+go
+
 CREATE TABLE tblPermissionAudit
 (
 dtTimeStamp		DateTime,
@@ -15,25 +19,48 @@ DDLCommand		NVARCHAR(MAX)
 
 GO
 
--- http://wutils.com/wmi/root/microsoft/sqlserver/serverevents/mssqlserver/ddl_database_security_events/
+use AW
+
+GO
 
 ALTER TRIGGER trgDBPermissionAudit   
 ON DATABASE   
 FOR DDL_DATABASE_SECURITY_EVENTS  
-AS   
---IF (@@ROWCOUNT = 0)
-	INSERT INTO AW..tblPermissionAudit
-	SELECT
-		 GetDate()
-		,EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/ServerName)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/LoginName)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/DatabaseName)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/ObjectType)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/RoleName)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/Grantees)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/Permissions)[1]','nvarchar(max)')  
-		,EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand)[1]','nvarchar(max)')  
+AS  
+-- http://wutils.com/wmi/root/microsoft/sqlserver/serverevents/mssqlserver/ddl_database_security_events/
+-- Track all events in [DDL_DATABASE_SECURITY_EVENTS] group and write them into csn_dba..tblPermissionAudit
+	DECLARE @ErrorMessage NVARCHAR(MAX)
+	BEGIN TRY
+		IF (OBJECT_ID('csn_dba..tblPermissionAudit')) IS NULL
+		BEGIN
+			RAISERROR ('csn_dba..tblPermissionAudit is not accessible' , 16, 1)  
+		END
+
+		INSERT INTO csn_dba..tblPermissionAudit -- Trigger will fail if user, who has invoked the permission change, has no rights to write into csn_dba..tblPermissionAudit
+		SELECT
+			 GetDate()
+			,EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/ServerName)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/LoginName)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/DatabaseName)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/ObjectType)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/RoleName)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/Grantees)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/Permissions)[1]','nvarchar(max)')  
+			,EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand)[1]','nvarchar(max)')  
+	END TRY 
+	BEGIN CATCH
+		SET @ErrorMessage = CONCAT('Audit Trigger failure: please investigate:',ERROR_MESSAGE()) 
+		RAISERROR (@ErrorMessage , 19, 1)  WITH LOG 
+	END CATCH
 GO  
+
+
+
+
+
+
+
+
 
