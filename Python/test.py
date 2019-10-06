ab3f8c0d20cd4780e886ed5646e7524399094d5e
Ilia Opiakin <gloom.fox@yandex.ru>
2019-10-06 16:53:34 +0200

    prime algorythm

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,26 @@
+import math
+
 primary1 = 43
 primary2 = 53
 message = primary1 * primary2
 print (message)
+
+i=50041401
+i_max = 50041501
+
+while i<i_max:
+    p = 3
+    stop = 0
+    while p<=math.sqrt(i) and not stop:
+        ost = i%p
+        if ost == 0:
+            print (i," Not primary, divider = ",p)
+            stop = 1
+        p=p+2
+    if not stop:
+        print (i, "is Prime")
+    i=i+2
+
 
 
 
