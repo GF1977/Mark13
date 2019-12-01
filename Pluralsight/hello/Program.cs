c8bc1569b2f9e082393fea125b7890850dde0db7
Ilia Opiakin <iopiakin@wayfair.com>
2019-12-01 17:42:04 +0100

    First console app

---
+++ Pluralsight/hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			if (args.Length > 0)
+			{
+				int a = int.Parse(args[0]);
+				int b = int.Parse(args[1]);
+				try
+				{
+					int c = a*b;
+				}
+				catch (Exception e) 
+				{
+					Console.WriteLine("{0} Exception caught.", e);
+				}
+				
+			}
+        }
+    }
+}
