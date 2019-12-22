2ad1168e300e616a066fb89b6a187353749848df
Ilia Opiakin <gloom.fox@yandex.ru>
2019-12-22 20:42:09 +0100

    Puzzle 7: Part Two is done!

--- .NET Core/Santa/Puzzle7/Puzzle7/Program.cs
+++ .NET Core/Santa/Puzzle7/Puzzle7/Program.cs
@@ -24,17 +24,18 @@ namespace Puzzle7
             return permutations;
         }
 
-        static int RunMyProgramm(List<int> commands2, int[] InputValue)
+        static Int64[] RunMyProgramm(List<Int64> commands2, Int64[] InputValue, Int64 Pointer)
         {
             int i = 0;
-            int nStep = 0;
+            if (Pointer > 0)
+                i = 1;
+            Int64 nStep = Pointer;
             bool bError = false;
-            int Output=-1;
+            Int64[] Output = { -1, nStep }; // Value , Pointer
             while (nStep <= commands2.Count && !bError)
             {
                 TheCommand myCommand = new TheCommand(nStep, ref commands2);
-                //Console.WriteLine("Pointer: {0}     Command: {1}", nStep, myCommand.GetCommand());
-                int stepIncrease = myCommand.GetStep();
+                Int64 stepIncrease = myCommand.GetStep();
                 switch (myCommand.GetCommand())
                 {
                     case 1: // Add
@@ -47,11 +48,13 @@ namespace Puzzle7
 
                     case 3: // Input
                         myCommand.Input(InputValue[i], commands2);
-                        i++;
+                        if (i < 1)
+                            i++;
                         break;
 
                     case 4: // Output
-                        Output = myCommand.Output();
+                        Output[0] = myCommand.Output();
+                        bError = true;
                         break;
 
                     case 5: //Opcode 5: jump-if-true:
@@ -74,6 +77,7 @@ namespace Puzzle7
 
                     case 99: // Halt
                         //Console.WriteLine("Case 99: Stop");
+                        nStep = 0;
                         bError = true;
                         break;
 
@@ -82,9 +86,9 @@ namespace Puzzle7
                         bError = true;
                         break;
                 }
-
                 nStep += stepIncrease;
             }
+            Output[1] = nStep;
             return Output;
         }
 
@@ -94,67 +98,72 @@ namespace Puzzle7
             string line = file.ReadLine();
             string[] words = line.Split(',');
 
-            List<int> commands_vanile = new List<int>();
+            List<Int64> commands_vanile = new List<Int64>();
 
             foreach (string word in words)
             {
-                commands_vanile.Add(int.Parse(word));
+                commands_vanile.Add(Int64.Parse(word));
             }
-
-
 
             string sPhaseSettings;
             int counter = 1;
-            int maxOutput = 0;
-            string sBestPhaseSettings="";
-            foreach (var p in Permutate("01234"))
+            Int64 maxOutput = 0;
+            string sBestPhaseSettings = "";
+            foreach (var p in Permutate("56789"))
             {
                 sPhaseSettings = p;
                 counter++;
-
-                int[] nTheValue = { 0, 0 };
-                int res = 0;
-                for (int i = 0; i < 5; i++)
+                Int64[] nTheValue = { 0, 0 };
+                int iterator = 0;
+                Int64[] res = { 0, 0 };
+                List<Int64>[] GeneratorMemory = { new List<Int64>(commands_vanile), new List<Int64>(commands_vanile), new List<Int64>(commands_vanile), new List<Int64>(commands_vanile), new List<Int64>(commands_vanile) };
+                Int64[] amPointer = { 0, 0, 0, 0, 0 };
+                while (res[0] >= 0)
                 {
-                    nTheValue[0] = int.Parse(sPhaseSettings[i].ToString());
-                    nTheValue[1] = res;
-                    List<int> commands2 = new List<int>(commands_vanile);
-                    res = RunMyProgramm(commands2, nTheValue);
-                }
-                if (res > maxOutput)
-                {
-                    maxOutput = res;
-                    sBestPhaseSettings = sPhaseSettings;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        List<Int64> commands2 = GeneratorMemory[i];
+                        nTheValue[0] = Int64.Parse(sPhaseSettings[i].ToString());
+                        nTheValue[1] = res[0];
+                        res = RunMyProgramm(commands2, nTheValue, amPointer[i]);
+                        amPointer[i] = res[1];
+                    }
+                    if (res[0] > maxOutput)
+                    {
+                        maxOutput = res[0];
+                        sBestPhaseSettings = sPhaseSettings;
+                    }
+                    iterator++;
                 }
             }
 
-            Console.WriteLine("PhaseSetting: {0}         Power: {1}", sBestPhaseSettings,maxOutput);
+            Console.WriteLine("PhaseSetting: {0}         Power: {1}", sBestPhaseSettings, maxOutput);
             return 0;
         }
 
         class TheCommand
         {
-            private static int command;
-            private static int Step;
+            private static Int64 command;
+            private static Int64 Step;
             struct myArgument
             {
-                public int argValue;
-                public int argMode;
+                public Int64 argValue;
+                public Int64 argMode;
             }
 
             private static myArgument ArgOne;
             private static myArgument ArgTwo;
             private static myArgument ArgThree;
-            public TheCommand(int nStep, ref List<int> words)
+            public TheCommand(Int64 nStep, ref List<Int64> words)
             {
-                int word = words[nStep];
+                Int64 word = words[(int)nStep];
                 command = word % 100;
                 Step = GetArgNumber(command) + 1; // 1 for command
                 word += 1000000;
                 if (word.ToString()[4] == '1') ArgOne.argMode = 1; else ArgOne.argMode = 0;
 
                 if (command != 99)
-                    ArgOne.argValue = ReadMemory(words[nStep + 1], ArgOne.argMode, ref words);
+                    ArgOne.argValue = ReadMemory(words[(int)(nStep + 1)], ArgOne.argMode, ref words);
 
                 if (command == 3)
                 {
@@ -164,17 +173,17 @@ namespace Puzzle7
                 if (GetArgNumber(command) >= 2)
                 {
                     if (word.ToString()[3] == '1') ArgTwo.argMode = 1; else ArgTwo.argMode = 0;
-                    ArgTwo.argValue = ReadMemory(words[nStep + 2], ArgTwo.argMode, ref words);
+                    ArgTwo.argValue = ReadMemory(words[(int)(nStep + 2)], ArgTwo.argMode, ref words);
 
                     if (GetArgNumber(command) >= 3)
                     {
                         ArgThree.argMode = 1;
-                        ArgThree.argValue = ReadMemory(words[nStep + 3], ArgThree.argMode, ref words);
+                        ArgThree.argValue = ReadMemory(words[(int)(nStep + 3)], ArgThree.argMode, ref words);
                     }
                 }
             }
             //Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter.Otherwise, it does nothing.
-            public int JumpIfTrue(int globalStep)
+            public Int64 JumpIfTrue(Int64 globalStep)
             {
                 if (ArgOne.argValue > 0)
                     Step = ArgTwo.argValue;
@@ -183,7 +192,7 @@ namespace Puzzle7
                 return Step;
             }
             //Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-            public int JumpIfFalse(int globalStep)
+            public Int64 JumpIfFalse(Int64 globalStep)
             {
                 if (ArgOne.argValue == 0)
                     Step = ArgTwo.argValue;
@@ -192,7 +201,7 @@ namespace Puzzle7
                 return Step;
             }
             //Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            public void LessThan(List<int> words)
+            public void LessThan(List<Int64> words)
             {
                 if (ArgOne.argValue < ArgTwo.argValue)
                     WriteMemory(ArgThree.argValue, 1, ref words);
@@ -201,7 +210,7 @@ namespace Puzzle7
             }
 
             //Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            public void Equals(List<int> words)
+            public void Equals(List<Int64> words)
             {
                 if (ArgOne.argValue == ArgTwo.argValue)
                     WriteMemory(ArgThree.argValue, 1, ref words);
@@ -209,37 +218,37 @@ namespace Puzzle7
                     WriteMemory(ArgThree.argValue, 0, ref words);
             }
 
-            public int GetCommand() { return command; }
-            public int GetStep() { return Step; }
-            public int ReadMemory(int mAddress, int mode, ref List<int> words)
+            public Int64 GetCommand() { return command; }
+            public Int64 GetStep() { return Step; }
+            public Int64 ReadMemory(Int64 mAddress, Int64 mode, ref List<Int64> words)
             {
-                int res;
+                Int64 res;
                 if (mode == 1) res = mAddress;
-                else res = words[mAddress];
+                else res = words[(int)(mAddress)];
                 return res;
             }
-            public void WriteMemory(int mAddress, int mvalue, ref List<int> words)
+            public void WriteMemory(Int64 mAddress, Int64 mvalue, ref List<Int64> words)
             {
-                words[mAddress] = mvalue;
+                words[(int)mAddress] = mvalue;
             }
-            public void Add(List<int> words)
+            public void Add(List<Int64> words)
             {
                 WriteMemory(ArgThree.argValue, ArgOne.argValue + ArgTwo.argValue, ref words);
             }
-            public void Multi(List<int> words)
+            public void Multi(List<Int64> words)
             {
                 WriteMemory(ArgThree.argValue, ArgOne.argValue * ArgTwo.argValue, ref words);
             }
-            public void Input(int number, List<int> words)
+            public void Input(Int64 number, List<Int64> words)
             {
                 WriteMemory(ArgOne.argValue, number, ref words);
             }
-            public int Output()
+            public Int64 Output()
             {
-               // Console.WriteLine(ArgOne.argValue);
+                // Console.WriteLine(ArgOne.argValue);
                 return ArgOne.argValue;
             }
-            public int GetArgNumber(int arg)
+            public int GetArgNumber(Int64 arg)
             {
                 int res = 0;
                 switch (arg % 100)
