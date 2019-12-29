b389e72a44ebc93125e839e48c68512ef0041e08
Ilia Opiakin <gloom.fox@yandex.ru>
2019-12-29 12:34:24 +0100

    Few cosmetic changes

--- .NET Core/Santa/Puzzle2/Puzzle2/Program.cs
+++ .NET Core/Santa/Puzzle2/Puzzle2/Program.cs
@@ -3,33 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using MyClasses;
 
+
+// https://adventofcode.com/2019/day/2
+
 namespace Puzzle2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Int64[] res;
-
             Int64 StartValue = 1;
-            StreamReader file = new StreamReader(@".\data.txt");
-            string line = file.ReadLine();
-            string[] words = line.Split(',');
+            StreamReader    file    = new StreamReader(@".\data.txt");
+            string          line    = file.ReadLine();
+            string[]        words   = line.Split(',');
 
             List<Int64> commands_vanile = new List<Int64>();
             foreach (string word in words)
-            {
                 commands_vanile.Add(Int64.Parse(word));
-            }
 
             List<Int64> commands = new List<Int64>(commands_vanile);
 
+
+            // Part One
             commands[1] = 12;
             commands[2] =  2;
 
             TheCommand.RunMyProgramm(commands, StartValue);
             Console.WriteLine("1st part Puzzle: {0}", commands[0]);
 
+            // Part two
             for (int n1 = 0; n1 < 100; n1++ )
                 for(int n2 = 0; n2 < 100; n2++)
                 {
