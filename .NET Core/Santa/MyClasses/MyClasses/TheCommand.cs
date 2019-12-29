ba3a2eab451ef146828e94a415b49ad295de6dd0
Ilia Opiakin <gloom.fox@yandex.ru>
2019-12-29 01:18:24 +0100

    Added "Run The Programm" functions into the "TheCommand" class.

--- .NET Core/Santa/MyClasses/MyClasses/TheCommand.cs
+++ .NET Core/Santa/MyClasses/MyClasses/TheCommand.cs
@@ -205,6 +205,76 @@ namespace MyClasses
             Console.WriteLine("---------------------------");
         }
 
+        public static Int64[] RunMyProgramm(List<Int64> commands2, Int64 InputValue)
+        {
+            Int64 nStep = 0;
+            bool bError = false;
+            Int64[] Output = { -1, nStep }; // Value , Pointer
+            while (nStep <= commands2.Count && !bError)
+            {
+                TheCommand myCommand = new TheCommand(nStep, ref commands2);
+                // myCommand.Debug();
+
+                Int64 stepIncrease = myCommand.GetStep();
+                switch (myCommand.GetCommand())
+                {
+                    case 1: // Add
+                        myCommand.Add(commands2);
+                        break;
+
+                    case 2: // Multi
+                        myCommand.Multi(commands2);
+                        break;
+
+                    case 3: // Input
+                        myCommand.Input(InputValue, commands2);
+                        break;
+
+                    case 4: // Output
+                        Output[0] = myCommand.Output();
+                        //bError = true;
+                        break;
+
+                    case 5: //Opcode 5: jump-if-true:
+                        nStep = myCommand.JumpIfTrue(nStep);
+                        stepIncrease = 0;
+                        break;
+
+                    case 6: //Opcode 6: jump-if-false
+                        nStep = myCommand.JumpIfFalse(nStep);
+                        stepIncrease = 0;
+                        break;
+
+                    case 7: //Opcode 7: less than
+                        myCommand.LessThan(commands2);
+                        break;
+
+                    case 8: //Opcode 8: equal
+                        myCommand.Equals(commands2);
+                        break;
+
+                    case 9: //Opcode 9: relative_base_offset adjustement
+                        myCommand.AdjustRelativeBaseOffset();
+                        break;
+
+
+                    case 99: // Halt
+                        //Console.WriteLine("Case 99: Stop");
+                        nStep = 0;
+                        bError = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Error");
+                        bError = true;
+                        break;
+                }
+                nStep += stepIncrease;
+            }
+            Output[1] = nStep;
+            return Output;
+        }
+
 
     }
 }
