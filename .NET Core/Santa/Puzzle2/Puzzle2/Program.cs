957b7e8bb6b7d683e43fd428f272c5bd2bbc27be
Ilia Opiakin <gloom.fox@yandex.ru>
2019-12-29 01:19:31 +0100

    Refactored puzzle 2: - now TheCommand class is used.

--- .NET Core/Santa/Puzzle2/Puzzle2/Program.cs
+++ .NET Core/Santa/Puzzle2/Puzzle2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using MyClasses;
 
 namespace Puzzle2
 {
@@ -8,95 +9,41 @@ namespace Puzzle2
     {
         static void Main(string[] args)
         {
-            int noun = 0;
-            int verb = 0;
-            Int64 res;
+            Int64[] res;
 
-            bool bStop = false;
-            
-
-            while (noun < 100 && !bStop)
-            {
-                while (verb < 100 && !bStop)
-                {
-                    res = RunTheProgramm(noun,verb);
-                    if (res == 19690720)
-                    {
-                        Console.WriteLine("Verb = {0};   Noun = {1};  Result = {2}; Answer = {3}", verb, noun,res, 100 * noun + verb);
-                        bStop = true;
-                        break;
-                    }
-                    else
-                    {
-                        //Console.WriteLine("Verb = {0};   Noun = {1};   Result = {2}", verb, noun, res);
-                        verb++;
-                    }
-                }
-                verb = 0;
-                noun++;
-            }
-
-
-
-            Console.WriteLine("1st part Puzzle: {0}", RunTheProgramm(12, 2));
-
-        }
-
-
-                     
-        static Int64 RunTheProgramm(int noun, int verb)
-        {
+            Int64 StartValue = 1;
             StreamReader file = new StreamReader(@".\data.txt");
             string line = file.ReadLine();
             string[] words = line.Split(',');
 
-            List<int> commands = new List<int>();
-            int i = 0;
-
-            while (i < words.Length)
+            List<Int64> commands_vanile = new List<Int64>();
+            foreach (string word in words)
             {
-                commands.Add(int.Parse(words[i++]));
+                commands_vanile.Add(Int64.Parse(word));
             }
 
-            // run the programm..
-            // replace position 1 with the value 12 and replace position 2 with the value 2
-            commands[1] = noun;
-            commands[2] = verb;
+            List<Int64> commands = new List<Int64>(commands_vanile);
 
-            i = 0;
-            bool bError = false;
-            while (i <= commands.Count && commands[i] != 99 && !bError)
-            {
-                int command = commands[i];
-                int n1 = commands[i + 1];
-                int n2 = commands[i + 2];
-                int n3 = commands[i + 3];
-                switch (command)
+            commands[1] = 12;
+            commands[2] =  2;
+
+            TheCommand.RunMyProgramm(commands, StartValue);
+            Console.WriteLine("1st part Puzzle: {0}", commands[0]);
+
+            for (int n1 = 0; n1 < 100; n1++ )
+                for(int n2 = 0; n2 < 100; n2++)
                 {
-                    case 1:
-                        //Console.WriteLine("Case 1: ADD   {0} + {1} -> {2}", commands[n1], commands[n2], i + 3 );
-                        commands[n3] = commands[n1] + commands[n2];
+                    commands = new List<Int64>(commands_vanile);
+                    commands[1] = n1;
+                    commands[2] = n2;
+                    TheCommand.RunMyProgramm(commands, StartValue);
+                    if (commands[0] == 19690720)
+                    {
+                        Console.WriteLine("2nd part Puzzle: {0}", 100*n1+n2);
                         break;
+                    }
 
-                    case 2:
-                        //Console.WriteLine("Case 2: Multiply {0} * {1} -> {2}", commands[n1], commands[n2], i + 3);
-                        commands[n3] = commands[n1] * commands[n2];
-                        break;
-
-                    case 99:
-                        // Console.WriteLine("Case 3: Stop");
-                        break;
-
-                    default:
-                        Console.WriteLine("Error");
-                        bError = true;
-                        break;
                 }
-
-                i += 4;
-            }
-            
-            return commands[0];
         }
     }
 }
