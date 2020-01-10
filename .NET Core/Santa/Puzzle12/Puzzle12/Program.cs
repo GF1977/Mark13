25a47940e5bfd72e8920fd2a875f67ad79fbf22b
Ilia Opiakin <gloom.fox@yandex.ru>
2020-01-10 22:38:28 +0100

    Puzzle 12: part 2 has been done!

--- .NET Core/Santa/Puzzle12/Puzzle12/Program.cs
+++ .NET Core/Santa/Puzzle12/Puzzle12/Program.cs
@@ -5,13 +5,13 @@ namespace Puzzle12
 {
     public class Moon
     {
-        public string name;
-        public int x;
-        public int y;
-        public int z;
-        public int velocity_x;
-        public int velocity_y;
-        public int velocity_z;
+         string name;
+         int x;
+         int y;
+         int z;
+         int velocity_x;
+         int velocity_y;
+         int velocity_z;
 
         public Moon(string name, int x, int y, int z)
         {
@@ -83,21 +83,6 @@ namespace Puzzle12
             return this.GetKineticakEnergy() * this.GetPotentialEnergy();
         }
 
-        public bool Compare(Moon planet)
-        {
-            // We don't compare names because they are not the same.
-            if (this.x == planet.x &&
-                    this.y == planet.y &&
-                    this.z == planet.z &&
-                    this.velocity_x == planet.velocity_x &&
-                    this.velocity_y == planet.velocity_y &&
-                    this.velocity_z == planet.velocity_z)
-
-                return true;
-            else
-                return false;
-        }
-
         public static void MoveThePlanets(List<Moon> Planets)
         {
             foreach (Moon planetA in Planets)
@@ -110,6 +95,50 @@ namespace Puzzle12
             }
         }
 
+        public static Int64 GetFullCycleForAxis (char cAxis, List<Moon> PlanetsVanile) // 'X' 'Y' 'Z'
+        {
+            int nTheVelocity = 0;
+            
+            List<Moon> Planets2;
+            Planets2 = new List<Moon>();
+            foreach (Moon planet in PlanetsVanile)
+                Planets2.Add(new Moon(planet));
+
+            Int64 nMovementCount = 0;
+            Int64 nCount = 0;
+            while (nCount != 4)
+            {
+                nMovementCount++;
+                nCount = 0;
+                Moon.MoveThePlanets(Planets2);
+                foreach (Moon planet in Planets2)
+                {
+                    switch (cAxis) // this looks ugly, ned to think about
+                    {
+                        case 'X':
+                            nTheVelocity = planet.velocity_x;
+                            break;
+                        case 'Y':
+                            nTheVelocity = planet.velocity_y;
+                            break;
+                        case 'Z':
+                            nTheVelocity = planet.velocity_z;
+                            break;
+                        default:
+                            Console.WriteLine("Argument Axis should be X|Y|Z");
+                            break;
+                    }
+                                
+                if (nTheVelocity == 0)
+                        nCount++;
+                    else
+                        break;
+                }
+            }
+
+            return nMovementCount;
+        }
+
     }
     class Program
     {
@@ -117,11 +146,16 @@ namespace Puzzle12
         {
             DateTime dateTime = DateTime.Now;
 
-            //int  steps = 1000;
-            //Moon Io = new Moon("Io", -3, 10, -1);
-            //Moon Europa = new Moon("Europa", -12, -10, -5);
-            //Moon Ganymede = new Moon("Ganymede", -9, 0, 10);
-            //Moon Callisto = new Moon("Callisto", 7, -5, -3);
+            int steps;
+            int nTotalEnergy = 0;
+
+            // My personal input:
+            // Answers: Total energy =  10944  / Total movement = 484244804958744
+            steps = 1000;
+            Moon Io = new Moon("Io", -3, 10, -1);
+            Moon Europa = new Moon("Europa", -12, -10, -5);
+            Moon Ganymede = new Moon("Ganymede", -9, 0, 10);
+            Moon Callisto = new Moon("Callisto", 7, -5, -3);
 
             //<x=  -3, y=  10, z= -1>
             //<x= -12, y= -10, z= -5>
@@ -129,7 +163,7 @@ namespace Puzzle12
             //<x=   7, y=  -5, z= -3>
 
             //Test Sample -10 runs. Answers: Total energy =  179  / Total movement = 2772
-            //int steps = 10;
+            //steps = 10;
             //Moon Io = new Moon("Io", -1, 0, 2);
             //Moon Europa = new Moon("Europa", 2, -10, -7);
             //Moon Ganymede = new Moon("Ganymede", 4, -8, 8);
@@ -137,11 +171,11 @@ namespace Puzzle12
 
 
             //test sample - 100 runs. Answers: Total energy =  1940  / Total movement = 4686774924
-            int steps = 100;
-            Moon Io = new Moon("Io", -8, -10, 0);
-            Moon Europa = new Moon("Europa", 5, 5, 10);
-            Moon Ganymede = new Moon("Ganymede", 2, -7, 3);
-            Moon Callisto = new Moon("Callisto", 9, -8, -3);
+            //steps = 100;
+            //Moon Io = new Moon("Io", -8, -10, 0);
+            //Moon Europa = new Moon("Europa", 5, 5, 10);
+            //Moon Ganymede = new Moon("Ganymede", 2, -7, 3);
+            //Moon Callisto = new Moon("Callisto", 9, -8, -3);
 
             //< x = -8, y = -10, z =  0 >
             //< x =  5, y =   5, z = 10 >
@@ -155,71 +189,50 @@ namespace Puzzle12
             PlanetsVanile.Add(Ganymede);
             PlanetsVanile.Add(Callisto);
 
-
             // First part of the puzzle;
             List<Moon> Planets = new List<Moon>();
             foreach (Moon planet in PlanetsVanile)
                 Planets.Add(new Moon(planet));
 
-
-            while (steps-->0)
-            {
+            while (steps-- > 0)
                 Moon.MoveThePlanets(Planets);
-            }
 
-            int nTotalEnergy=0;
             foreach (Moon planet in Planets)
                 nTotalEnergy += planet.GetTotalEnergy();
 
             Console.WriteLine("Total energy = {0}", nTotalEnergy);
 
             // Second part of the puzzle;
-            //List<Moon> Planets2 = new List<Moon>();
-            //foreach (Moon planet in PlanetsVanile)
-            //    Planets2.Add(new Moon(planet));
 
-            Int64 nMovementCount;
-            //while(i != Planets2.Count) // i represents the number of moons with the exactly the same parameters
-            //{
-            //    i = 0;
-            //    Moon.MoveThePlanets(Planets2);
-            //    foreach (Moon planet in Planets2)
-            //        if(planet.Compare(PlanetsVanile[i]))
-            //            i++;
-            //        else
-            //            break;
-            //    nMovementCount++;
-            //}
+            Int64 nX = Moon.GetFullCycleForAxis('X', PlanetsVanile);
+            Console.WriteLine("Number of cycles: {0}", nX);
 
+            Int64 nY = Moon.GetFullCycleForAxis('Y', PlanetsVanile);
+            Console.WriteLine("Number of cycles: {0}", nY);
+
+            Int64 nZ = Moon.GetFullCycleForAxis('Z', PlanetsVanile);
+            Console.WriteLine("Number of cycles: {0}", nZ);
+
+            Console.WriteLine("Final Answer: {0}", GetLCM(nX, nY, nZ) * 2); // Need to explain why  * 2 
+            Console.WriteLine("----------------------------------------------");
             Console.WriteLine("Execution time: {0}", DateTime.Now - dateTime);
         }
 
-        static Int64 IsGood(Int64 a, Int64 b, Int64 c, Int64 d)
+        static Int64 GetLCM(Int64 a, Int64 b, Int64 c) //Least common multiple
         {
-            Int64 res = 0;
-            if (a == 0 || b == 0 || c == 0 || d == 0)
-                return 0;
+            Int64 nMax;
+            
+            nMax = Math.Max(a, b);
+            for (Int64 i = 2; i <= Math.Sqrt(nMax); i++)
+                while (a % i == 0 && b % i == 0) { a /= i; }
 
-            Int64 nMin = Math.Min(Math.Min(a, b), Math.Min(c, d));
-            if (a % nMin == 0 && b % nMin == 0 && c % nMin == 0 && d % nMin == 0)
-                res = nMin;
+            nMax = Math.Max(b, c);
+            for (Int64 i = 2; i <= Math.Sqrt(nMax); i++)
+                while (b % i == 0 && c % i == 0) { b /= i; }
 
-            return res;
-        }
-
-        static Int64 GetNOK(Int64 a, Int64 b, Int64 c)
-        {
-            Int64 nMax = Math.Max(Math.Max(a, b), c);
-
-            for (Int64 i = 2;i < nMax; i++)
-            {
-                while (a % i == 0 && b % i ==0 && c % i ==0)
-                {
-                    a /= i;
-                    b /= i;
-                    c /= i;
-                }
-            }
+            nMax = Math.Max(a, c);
+            for (Int64 i = 2; i <= Math.Sqrt(nMax); i++)
+                while (a % i == 0 && c % i == 0) { c /= i; }
 
             return a*b*c;
         }
