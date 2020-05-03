126aee21ed5dcb5cdfd2136cd329bb50c128d69f
Ilia Opiakin <gloom.fox@yandex.ru>
2020-05-03 12:12:25 +0200

    Prepared the template for Intercode application

--- .NET Core/Santa/Puzzle19/Program.cs
+++ .NET Core/Santa/Puzzle19/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Puzzle19
 {
@@ -6,7 +8,20 @@ namespace Puzzle19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Int64 StartValue = 1;    // Part #1 
+
+            StreamReader file = new StreamReader(@".\data.txt");
+            string line = file.ReadLine();
+            string[] words = line.Split(',');
+
+            List<Int64> commands_vanile = new List<Int64>();
+            foreach (string word in words)
+                commands_vanile.Add(Int64.Parse(word));
+
+            for (int ii = 0; ii < 1000; ii++)
+                commands_vanile.Add(0);
+
+            List<Int64> commands = new List<Int64>(commands_vanile);
         }
     }
 }
