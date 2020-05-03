e8236192a304e5579658bfe60f72eb69db7286e2
Ilia Opiakin <gloom.fox@yandex.ru>
2020-05-03 12:45:09 +0200

    Template is ready

--- .NET Core/Santa/MyClass-template/Program.cs
+++ .NET Core/Santa/MyClass-template/Program.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using MyClasses;
 
-namespace MyClass-template
+namespace MyClassTemplate
 {
     class Program
     {
+
+        static Int64 nProgrammStep = 0;
+        static List<Int64> commands;
+        static List<Int64> commands2;
+
+        static void RunTheProgramm(Int64 nStartValue)
+        {
+            Int64 nStatus;
+            do
+            {
+                TheCommand myCommand = new TheCommand(nProgrammStep, ref commands);
+                Int64[] res = myCommand.ExecuteOneCommand(nProgrammStep, nStartValue, commands);
+                nStatus = res[0];
+                nProgrammStep = res[1];
+
+                if (nStatus == 10)
+                {
+                    Console.WriteLine("");
+                }
+                else if (nStatus > 20 && nStatus < 128)
+                {
+                    Console.Write((char)nStatus);
+                }
+            }
+            while (nProgrammStep != 0);
+        }
+
         static void Main(string[] args)
         {
             Int64 StartValue = 1;    // Part #1 
@@ -22,6 +50,13 @@ namespace MyClass-template
                 commands_vanile.Add(0);
 
             List<Int64> commands = new List<Int64>(commands_vanile);
+
+            RunTheProgramm(StartValue);
         }
+
+
+
+ 
+
     }
 }
