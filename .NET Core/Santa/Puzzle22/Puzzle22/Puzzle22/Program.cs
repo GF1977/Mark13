a755a360db39797f859d604e68d44d4e15495c1b
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-11-28 17:22:57 +0100

    SOme experiments with reverse engineering. It is dead end, need to use math

--- .NET Core/Santa/Puzzle22/Puzzle22/Puzzle22/Program.cs
+++ .NET Core/Santa/Puzzle22/Puzzle22/Puzzle22/Program.cs
@@ -7,81 +7,115 @@ namespace Puzzle22
 {
     class Program
     {
-        static readonly Int64 nMaxCards = 10007;  //119315717514047 - for part 2  // standard is 10007
-        static readonly Int64 nIterations  = 101741582076661; // Part 2 - how many iterations we need to do
+        static readonly Int64 nMaxCards = 119315717514047; // for part 2  // standard is 10007  // 119315717514047
+                                                     //  11111113
+        static readonly Int64 nIterations = 101741582076661; // Part 2 - how many iterations we need to do // 101741582076661
 
 
         static void Main()
         {
-            int nCardValue = 7777;
-            Int64 nCardPosition = 2020;
+            int nCardValue = 2019;
+            Int64 nCardPosition;
 
 
 
             List<string> commands = new List<string>();
             List<int> cards = new List<int>();
-            for (int i = 0; i < nMaxCards; i++)
+            for (int i = 0; i < 10007; i++)
                 cards.Add(i);
 
 
             foreach (string line in File.ReadLines(@".\data.txt"))
                 commands.Add(line);
 
+            Int64 N = 0;
 
             // PART #1
             Console.WriteLine("Part One");
-            foreach (string line in commands)
+            Int64 nTempPosition = 0;
+
+            //while (true)
             {
-                string[] words = line.Split(' ');
-                switch (words[0])
+                foreach (string line in commands)
                 {
-                    case "deal":
-                        if (words[1] == "with")
-                            DealWithIncremental(ref cards, int.Parse(words[3]));
-                        else
-                            NewStack(ref cards);
-                        break;
-                    case "cut":
-                        Cut(ref cards, int.Parse(words[1]));
-                        break;
+                    string[] words = line.Split(' ');
+                    switch (words[0])
+                    {
+                        case "deal":
+                            if (words[1] == "with")
+                                DealWithIncremental(ref cards, int.Parse(words[3]));
+                            else
+                                NewStack(ref cards);
+                            break;
+                        case "cut":
+                            Cut(ref cards, int.Parse(words[1]));
+                            break;
+                    }
+
+
+                    nCardPosition = cards.FindIndex(n => n == nCardValue);
+                    Console.WriteLine("Card {0} is on position {1}", nCardValue, nCardPosition);
+
                 }
-
                 nCardPosition = cards.FindIndex(n => n == nCardValue);
-              //  Console.WriteLine("Card {0} is on position {1}", nCardValue, nCardPosition);
+                Console.WriteLine("-------------------------------------------");
 
+                if (nCardPosition == 1867 && N >0)
+                {
+                    Console.WriteLine("-----------------  N = {0} --------------------------", N);
+                    //break;
+                }
+                N++;
+               
+                //Console.WriteLine("Card {0} is on position {1}", nCardValue, nCardPosition);
             }
-            nCardPosition = cards.FindIndex(n => n == nCardValue);
-            //Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("Card {0} is on position {1}", nCardValue, nCardPosition);
-
             // PART #2
             // Reverse order
             Console.WriteLine();
             Console.WriteLine("Part Two");
 
 
-            Int64 nTempPosition = nCardPosition;
-            for (int i = commands.Count - 1; i>=0;i--)
-            {
-                //Console.WriteLine("Checking position {0}", nCardPosition);
-                string line = commands[i];
-                string[] words = line.Split(' ');
-                switch (words[0])
-                {
-                    case "deal":
-                        if (words[1] == "with")
-                            nCardPosition = DealWithIncrementalReverse(nCardPosition, int.Parse(words[3]));
-                        else
-                            nCardPosition = NewStackReverse(nCardPosition);
-                        break;
-                    case "cut":
-                        nCardPosition = CutReverse(nCardPosition, int.Parse(words[1]));
-                        break;
-                }
-            }
-            //Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("On position {0} is Card {1}", nTempPosition, nCardPosition);
 
+            nCardPosition = 2020;
+            nTempPosition = nCardPosition;
+
+            Int64 nTempCard = 0;
+
+            while (N< nIterations)
+            {
+                for (int i = commands.Count - 1; i >= 0; i--)
+                {
+                    //Console.WriteLine("Checking position {0}", nCardPosition);
+                    string line = commands[i];
+                    string[] words = line.Split(' ');
+                    switch (words[0])
+                    {
+                        case "deal":
+                            if (words[1] == "with")
+                                nCardPosition = DealWithIncrementalReverse(nCardPosition, int.Parse(words[3]));
+                            else
+                                nCardPosition = NewStackReverse(nCardPosition);
+                            break;
+                        case "cut":
+                            nCardPosition = CutReverse(nCardPosition, int.Parse(words[1]));
+                            break;
+                    }
+                }
+                N++;
+
+
+                //if (nCardPosition == nTempPosition)
+                {
+                    //Console.WriteLine("-----------------  N = {0} --------------------------", N);
+                    //break;
+                }
+
+                //if (nTempPosition == 2020)
+                //  nTempPosition = nCardPosition;
+                Console.WriteLine("On position {0} is Card {1}", 2020, nCardPosition);
+
+            }
+            Console.WriteLine("On position {0} is Card {1}", nTempPosition, nCardPosition);
         }
 
         static Int64 NewStackReverse(Int64 nPosition)
@@ -98,15 +132,22 @@ namespace Puzzle22
 
         static Int64 DealWithIncrementalReverse(Int64 nPosition, int nIncrement)
         {
-            for (Int64 i = 0; i < nMaxCards; i++)
-            {
-                if ((i * nIncrement) % nMaxCards == nPosition)
-                {
-                    nPosition = i;
-                    break;
-                }
-            }
-            return nPosition;
+            //for (Int64 i = 0; i < nMaxCards; i++)
+            //{
+            //    if ((i * nIncrement) % nMaxCards == nPosition)
+            //    {
+            //        nPosition = i;
+            //        break;
+            //    }
+            //}
+
+
+            Int64 n = 0;
+            Int64 nOst = nMaxCards % nIncrement;
+            while((n * nOst + nPosition) % nIncrement != 0)
+                n++;
+
+            return (Int64)(n * nMaxCards + nPosition) /nIncrement;
         }
 
         static void DealWithIncremental(ref List<int> cards, int nIncrement)
