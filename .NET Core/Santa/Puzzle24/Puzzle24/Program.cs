3c9eda5960320f17b4800a36ee42e2dc4f771b10
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-11-29 21:49:06 +0100

    part one done

--- .NET Core/Santa/Puzzle24/Puzzle24/Program.cs
+++ .NET Core/Santa/Puzzle24/Puzzle24/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 
 namespace Puzzle24
 {
@@ -8,6 +9,8 @@ namespace Puzzle24
     {
         static void Main(string[] args)
         {
+            bool bStop = false;
+
             StreamReader file = new StreamReader(@".\data.txt");
             string line = file.ReadLine();
 
@@ -30,8 +33,59 @@ namespace Puzzle24
             }
 
 
+            Int64 nBioDiversity = 0;
+            List<Int64> lBioDiversity = new List<Int64>();
 
-            ShowTerra(Terra, nTerraSize);
+
+            while (!bStop)
+            {
+                //ShowTerra(Terra, nTerraSize);
+                //Console.ReadKey();
+                nBioDiversity = GetBiodiversity(Terra, nTerraSize);
+                Int64 x = lBioDiversity.FindIndex(n => n == nBioDiversity); 
+                if (x>=0)
+                    {
+                        Console.WriteLine("Part ONE: {0}", nBioDiversity);
+                        bStop = true;
+                    }
+
+                lBioDiversity.Add(nBioDiversity);
+
+
+                for (int r = 0; r < nTerraSize; r++)
+                    for (int c = 0; c < nTerraSize; c++)
+                    {
+                        int nNeighbors = 0;
+
+                        //if (IsValidCoordinate(r + 1, c + 1, nTerraSize) && Terra[r + 1, c + 1] == '#') nNeighbors++;
+                        if (IsValidCoordinate(r + 1, c + 0, nTerraSize) && Terra[r + 1, c + 0] == '#') nNeighbors++;
+                        //if (IsValidCoordinate(r + 1, c - 1, nTerraSize) && Terra[r + 1, c - 0]  == '#') nNeighbors++;
+
+                        if (IsValidCoordinate(r + 0, c + 1, nTerraSize) && Terra[r + 0, c + 1] == '#') nNeighbors++;
+                        if (IsValidCoordinate(r + 0, c - 1, nTerraSize) && Terra[r + 0, c - 1] == '#') nNeighbors++;
+
+
+
+                        //if (IsValidCoordinate(r - 1, c + 1, nTerraSize) && Terra[r - 1, c + 1] == '#') nNeighbors++;
+                        if (IsValidCoordinate(r - 1, c + 0, nTerraSize) && Terra[r - 1, c + 0] == '#') nNeighbors++;
+                        //if (IsValidCoordinate(r - 1, c - 1, nTerraSize) && Terra[r - 1, c - 1] == '#') nNeighbors++;
+
+
+                        TerraNew[r, c] = Terra[r, c];
+
+                        if (nNeighbors != 1 && Terra[r, c] == '#')
+                            TerraNew[r, c] = '.';
+
+                        if ((nNeighbors == 1 || nNeighbors == 2) && Terra[r, c] == '.')
+                            TerraNew[r, c] = '#';
+                    }
+
+                Terra = CopyTerra(TerraNew, nTerraSize);
+                
+            }
+
+
+
 
 
 
@@ -42,6 +96,19 @@ namespace Puzzle24
             Console.WriteLine("Press any key");
             Console.ReadKey();
         }
+
+        private static Int64 GetBiodiversity(char[,] Terra, int nTerraSize)
+        {
+            Int64 Result =0;
+
+            for (int r = 0; r < nTerraSize; r++)
+                for (int c = 0; c < nTerraSize; c++)
+                    if (Terra[r, c] == '#')
+                        Result += (Int64)Math.Pow(2, r * nTerraSize + c);
+
+            return Result;
+        }
+
 
         private static bool IsValidCoordinate(int x, int y, int nArraySize)
         {
