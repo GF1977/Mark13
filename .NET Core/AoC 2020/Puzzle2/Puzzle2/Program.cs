11eac6525ed6f1fe9c2e75a8304ec68d6ae0f1b8
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-12-05 14:58:08 +0100

    Part two done

--- .NET Core/AoC 2020/Puzzle2/Puzzle2/Program.cs
+++ .NET Core/AoC 2020/Puzzle2/Puzzle2/Program.cs
@@ -13,32 +13,43 @@ namespace Puzzle1
             int nMinCount;
             int nMaxCount;
 
-            char sABC;
+            char cABC;
             string sPassword;
 
 
             public PasswordPolicy(string sRawData)
             {
                 string[] sArray = sRawData.Split(' ','-' ,':');
-                this.nMinCount = int.Parse(sArray[0]);
-                this.nMaxCount = int.Parse(sArray[1]);
-                this.sABC = sArray[2][0]; // [0] means first char of the string
-                this.sPassword = sArray[4]; // [3] is an empty cell
+                nMinCount = int.Parse(sArray[0]);
+                nMaxCount = int.Parse(sArray[1]);
+                cABC = sArray[2][0]; // [0] means first char of the string
+                sPassword = sArray[4]; // [3] is an empty cell
 
             }
 
             public bool CheckPolicy()
             {
                 bool bRes = false;
-                // check the min
-                int nABCCount = this.sPassword.Count(n => n == sABC);
+                int nABCCount = sPassword.Count(n => n == cABC);
 
                 if (nABCCount >= nMinCount && nABCCount <= nMaxCount)
                     bRes = true;
 
                 return bRes;
-
             }
+
+
+            public bool CheckPolicyTCAS() //Toboggan Corporate Authentication System
+            {
+                bool bRes = false;
+
+                if ((cABC == sPassword[nMinCount-1] && cABC != sPassword[nMaxCount - 1]) || (cABC != sPassword[nMinCount-1] && cABC == sPassword[nMaxCount - 1]))
+                    bRes = true;
+
+                return bRes;
+            }
+
+
 
             public string GetPass()
             {
@@ -68,20 +79,22 @@ namespace Puzzle1
             }
 
             int nGoodPasswords = 0;
-            foreach(PasswordPolicy P in PassList)
+            int nGoodPasswordsTCAS = 0;
+
+            foreach (PasswordPolicy P in PassList)
             {
                 if (P.CheckPolicy())
-                {
-                    Console.WriteLine("Password: {0} - PASSED", P.GetPass());
                     nGoodPasswords++;
-                }
-                else
-                    Console.WriteLine("Password: {0} - FAILED", P.GetPass());
+
+                if (P.CheckPolicyTCAS())
+                    nGoodPasswordsTCAS++;
+
 
             }
 
             Console.WriteLine("--------------------------");
-            Console.WriteLine("Valid passwords: {0}", nGoodPasswords);
+            Console.WriteLine("Valid passwords       : {0}", nGoodPasswords);
+            Console.WriteLine("Valid passwords (TCAS): {0}", nGoodPasswordsTCAS);
 
         }
     }
