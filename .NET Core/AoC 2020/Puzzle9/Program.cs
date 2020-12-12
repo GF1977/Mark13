e5e668ee2d6d9a74e082421314a24105c53862d7
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-12-12 22:34:23 +0100

    Puzzle9: part two done

--- .NET Core/AoC 2020/Puzzle9/Program.cs
+++ .NET Core/AoC 2020/Puzzle9/Program.cs
@@ -9,45 +9,62 @@ namespace Puzzle9
 {
 
     const int PREAMBULA = 25;
-    static List<Int64> fileInput = new List<Int64>();
-    static void Main(string[] args)
+    static readonly List<Int64> EncryptedQueue = new List<Int64>();
+    static void Main()
     {
         Console.Clear();
         Console.WriteLine(DateTime.Now);
-        StreamReader file = new StreamReader(@".\data.txt");
 
-        var vPartOneAnswer = "";
-        var vPartTwoAnswer = "";
-
-
-
-        while (!file.EndOfStream)
-        {
-            string S = file.ReadLine();
-            fileInput.Add(Int64.Parse(S));
-        }
+            using (StreamReader file = new StreamReader(@".\data.txt"))
+            while (!file.EndOfStream)
+                {
+                    string S = file.ReadLine();
+                    EncryptedQueue.Add(Int64.Parse(S));
+                }
 
 
-            for (int i = PREAMBULA; i < fileInput.Count; i++)
+            for (int i = PREAMBULA; i < EncryptedQueue.Count; i++)
             {
-                if(!IsValidNumber(i))
-                    Console.WriteLine("PartOne: {0}", fileInput[i]);
-            }
+                if (!IsValidNumber(i))
+                {
+                    Console.WriteLine("PartOne: {0}", EncryptedQueue[i]);
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("PartTwo: {0}", GetWeakSet(EncryptedQueue[i]));
 
-        Console.WriteLine("--------------------------");
-//        Console.WriteLine("PartOne: {0}", vPartOneAnswer);
-//        Console.WriteLine("PartTwo: {0}", vPartTwoAnswer);
+                }
+            }
 
     }
 
-    static bool IsValidNumber(int N)
+    static bool IsValidNumber(int nPosition)
         {
-            
-            for (int x = N - PREAMBULA; x < N; x++)
-                for (int y = x + 1; y < N; y++)
-                    if (fileInput[N] == fileInput[x] + fileInput[y]) return true;
+            for (int x = nPosition - PREAMBULA; x < nPosition; x++)
+                for (int y = x + 1; y < nPosition; y++)
+                    if (EncryptedQueue[nPosition] == EncryptedQueue[x] + EncryptedQueue[y]) return true;
             
             return false;
         }
-}
+
+
+        static Int64 GetWeakSet(Int64 NotValidNumber)
+        {
+            for (int x = 0; x < EncryptedQueue.Count; x++)
+            {
+                Int64 nMin = Int64.MaxValue; ;
+                Int64 nMax = 0;
+
+                Int64 nSum = 0;
+                for(int y = x; y < EncryptedQueue.Count;y++)
+                {
+                    if (EncryptedQueue[y] > nMax) nMax = EncryptedQueue[y];
+                    if (EncryptedQueue[y] < nMin) nMin = EncryptedQueue[y];
+
+                    nSum += EncryptedQueue[y];
+
+                    if (nSum == NotValidNumber)  return nMin + nMax;
+                }
+            }
+            return -1;
+        }
+    }
 }
