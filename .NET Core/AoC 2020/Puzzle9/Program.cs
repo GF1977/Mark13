cc4fb32a385e98577c33f857b2d91e02f4eb2446
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-12-12 21:57:21 +0100

    Puzzle9: part one done (no optimization)

---
+++ .NET Core/AoC 2020/Puzzle9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Puzzle9
+{
+    class Program
+{
+
+    const int PREAMBULA = 25;
+    static List<Int64> fileInput = new List<Int64>();
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine(DateTime.Now);
+        StreamReader file = new StreamReader(@".\data.txt");
+
+        var vPartOneAnswer = "";
+        var vPartTwoAnswer = "";
+
+
+
+        while (!file.EndOfStream)
+        {
+            string S = file.ReadLine();
+            fileInput.Add(Int64.Parse(S));
+        }
+
+
+            for (int i = PREAMBULA; i < fileInput.Count; i++)
+            {
+                if(!IsValidNumber(i))
+                    Console.WriteLine("PartOne: {0}", fileInput[i]);
+            }
+
+        Console.WriteLine("--------------------------");
+//        Console.WriteLine("PartOne: {0}", vPartOneAnswer);
+//        Console.WriteLine("PartTwo: {0}", vPartTwoAnswer);
+
+    }
+
+    static bool IsValidNumber(int N)
+        {
+            
+            for (int x = N - PREAMBULA; x < N; x++)
+                for (int y = x + 1; y < N; y++)
+                    if (fileInput[N] == fileInput[x] + fileInput[y]) return true;
+            
+            return false;
+        }
+}
+}
