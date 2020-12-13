854bd6a0ef2a4136a65612ac17d65a3f7af359aa
ILYA OPYAKIN <i.opyakin@outlook.com>
2020-12-13 08:41:45 +0100

    Puzzle10: Part two done

--- .NET Core/AoC 2020/Puzzle10/Program.cs
+++ .NET Core/AoC 2020/Puzzle10/Program.cs
@@ -13,7 +13,6 @@ namespace Puzzle10
         Console.Clear();
         Console.WriteLine(DateTime.Now);
 
-        var vPartTwoAnswer = "";
 
         using (StreamReader file = new StreamReader(@".\data.txt"))
         while (!file.EndOfStream)
@@ -22,12 +21,55 @@ namespace Puzzle10
                     Adapters.Add(int.Parse(S));
         }
 
+            Adapters.Add(0);
             Adapters.Sort();
+
+            List<int> Chain = new List<int>();
+            int i = 0;
+
+            Int64 PartTwoAnswer = 1;
+
+            // we need to find all consequences with Len >= 3, as them can be replaced by other consequences
+            // 1,2,3 = 1,2,3 ; 1,3
+            // 1,2,3,4 = 1,2,3,4 ; 1,2,4 ; 1,3,4; 1,4
+            while (i<Adapters.Count-1)
+            {
+                while (i < Adapters.Count - 1 && Adapters[i] == Adapters[i + 1]-1)
+                    Chain.Add(Adapters[i++]);
+
+                Chain.Add(Adapters[i]);
+
+                if (Chain.Count >= 3)
+                {
+                    // did some pen & paper research to get those magic numbers
+                    // the piece of chain with Len = X can be replaced by Y combinations
+                    // X = 4 -> Y = 4
+                    // 1 2 3 4 ; 1 2 4 ; 1 3 4; 1 4
+                    switch (Chain.Count)
+                    {
+                        case 3:
+                            PartTwoAnswer *= 2; // Y = 2
+                            break;
+                        case 4:
+                            PartTwoAnswer *= 4; // Y = 4
+                            break;
+                        case 5:
+                            PartTwoAnswer *= 7; // Y = 7
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                }
+                Chain.Clear();
+                i++;
+            }
 
 
         Console.WriteLine("--------------------------");
         Console.WriteLine("PartOne: {0}", JoltsDiff());
-        Console.WriteLine("PartTwo: {0}", vPartTwoAnswer);
+        Console.WriteLine("PartTwo: {0}", PartTwoAnswer);
 
     }
 
